3e303a58ace64da88e73e2183f6263f0142dfb73
mokshith rao <mokshithrao1481@gmail.com>
2025-02-17 20:06:30 +0530

    signUp user

--- frontend/src/scripts/main.js
+++ frontend/src/scripts/main.js
@@ -4,5 +4,20 @@ const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
 const supabaseKey = import.meta.env.VITE_SUPABASE_KEY;
 
 const supabase = createClient(supabaseUrl, supabaseKey);
+let signUpBtn = document.querySelector('main .container form button[type="submit"]');
 
-console.log(supabase);
+async function signInUser() {
+    let formData = new FormData(document.getElementById('sign-in-form'));
+    let email = formData.get('email');
+    let password = formData.get('password');
+    const { data, error } = await supabase.auth.signUp({
+        email: email,
+        password: password
+    });
+}
+
+function main() {
+    if(signUpBtn) {
+        signUpBtn.addEventListener('click', signInUser);
+    }
+}
