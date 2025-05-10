e27498de6175ffb58b2ea4a272ee96982843d7b4
mokshith rao <mokshithrao1481@gmail.com>
2025-05-10 19:24:39 +0530

    initial commit

---
+++ reactClassroom/my-tailwind-project/src/App.jsx
@@ -0,0 +1,14 @@
+import {Button, NavButton} from "./components/button";
+import Heading from "./components/Heading";
+import Tagline from "./components/Tagline";
+import Nav from "./UI/Nav"
+import Hero from "./UI/Hero";
+function App() {
+    return (
+        <>
+        <Nav></Nav>
+        <Hero></Hero>
+        </>
+    );
+}
+export default App;
---
+++ reactClassroom/my-tailwind-project/src/components/SkillsCard.jsx
@@ -0,0 +1,22 @@
+import Tagline from "./Tagline";
+import { FileCode } from "lucide-react"; // HTML-Icon
+import { Braces } from "lucide-react"; //CSS-Icon
+import { Database } from "lucide-react"; //Supabase-Icon
+import { FileJson } from "lucide-react"; //JavaScript-Icon
+import { GitGraph } from "lucide-react"; //Git-Icon
+import { SquareCode } from "lucide-react"; //Python-Icon
+import { Computer } from "lucide-react"; //Linux-Icon
+import { Cpu } from "lucide-react"; //Machine-learning-Icon
+import { Blocks } from "lucide-react"; //react-Icon
+import { Hexagon } from "lucide-react"; //Node-Icon
+const skillsData = {
+
+}
+
+function SkillsCard() {
+    return(
+        <div>
+            <Tagline label="My Skills"></Tagline>
+        </div>
+    );
+}
